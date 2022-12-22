0ef50e3de44769122ac9f0f0ff2bab41c6064bf3
Aditya6174 <96608682+Aditya6174@users.noreply.github.com>
2022-12-22 10:48:57 +0530

    Update rockPaperScissors.py

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -1,4 +1,5 @@
 import random
+# We input our choice of rock,paper or scissors and the computer chooses one and finally the winner is decided .
 def lessPlay():
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r' , 'p' , 's'])
@@ -9,7 +10,7 @@ def lessPlay():
         return "you won buddy !"
 
     return "you lost , computer won"
-#r > s s > p p > r
+# r > s s > p p > r
 
 def winner(player,computer) :
     if (player == 'r' and computer == 's') or (player == 's' and computer == 'p') or (player == 'p' and computer == 'r') :
